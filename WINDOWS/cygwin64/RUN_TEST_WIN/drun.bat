7a4ed8afdd6a8108638b5260aa2978098bb49257
Federica Bertolotti <federica.bertolotti@uninsubria.it>
2022-05-02 11:07:37 +0200

    aa

--- WINDOWS/cygwin64/RUN_TEST_WIN/drun.bat
+++ WINDOWS/cygwin64/RUN_TEST_WIN/drun.bat
@@ -1,4 +1,6 @@
 echo off
+C:\cygwin64\DEBUSSY_v2.2\bin\cif2pha_DebUsSy.exe magnetite.cif magnetite.pha
+C:\cygwin64\DEBUSSY_v2.2\bin\DB_PHA_CLU_x1.0.exe magnetite.ddb
 C:\cygwin64\DEBUSSY_v2.2\bin\MK_CELL_x1.0
 C:\cygwin64\DEBUSSY_v2.2\bin\MK_BALL_x1.0 
 C:\cygwin64\DEBUSSY_v2.2\bin\Debussy Fe3O4.dwa
